b350eb289b8fd8c2002ac2f0b84d69cae0640545
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-08 17:04:27 +0100

    Middleware config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { join } from 'path';
+import session from 'express-session';
 
 export const app = express();
+
+// Middleware configuration
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(join(__dirname, '../public')));
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET as string,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
