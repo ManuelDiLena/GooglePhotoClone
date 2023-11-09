5b3b8cf3e3e22998c48be8e5460ebb32c3cf9af5
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-09 17:54:31 +0100

    Home rute

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -3,5 +3,9 @@ import express, { NextFunction, Request, Response } from 'express';
 export const router = express.Router();
 
 router.get('/home', (req: Request, res: Response) => {
+    res.render('home/index', { user: req.session.user });
+})
+
+router.post('/upload', (req: Request, res: Response) => {
     res.render('home/index');
 })
