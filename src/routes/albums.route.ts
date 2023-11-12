56de3afaba9baaa0ab782f35b4530555d01d96b2
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-11-12 11:28:45 +0100

    Show photos from each album

--- src/routes/albums.route.ts
+++ src/routes/albums.route.ts
@@ -1,12 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
 import Album, { IAlbum } from '../model/album.model';
+import Photo, { IPhotoReq, IPhoto } from '../model/photo.model';
 
 export const router = express.Router();
 
 // Route to show all albums
 router.get('/albums', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const albums = await Album.find({ userid: req.session.user._id });
+        const albums = await Album.find({ userid: req.session.user._id! });
         res.render('albums/index', { user: req.session.user, albums });
 
     } catch (err) {
@@ -14,8 +15,37 @@ router.get('/albums', async (req: Request, res: Response, next: NextFunction) =>
     }
 });
 
-// Route to view photos from a specific algum
-router.get('/albums/:id', (req: Request, res: Response, next: NextFunction) => {});
+// Route to view photos from a specific album
+router.get('/albums/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const albumid = req.params.id;
+
+    try {
+        let photos = await Photo.find({
+            albums: albumid,
+        });
+
+        let album = await Album.findById(albumid);
+
+        if (album) {
+            if (album.userid !== req.session.user._id && album.isprivate) {
+                res.render('error/privacy', {});
+                return;
+            }
+        }
+
+        const albums = await Album.find({ userid: req.session.user._id! });
+
+        res.render('albums/view', {
+            user: req.session.user,
+            photos,
+            album,
+            albums,
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 // Route to create a new album in the DB
 router.post('/create-album', async (req: Request, res: Response, next: NextFunction) => {
